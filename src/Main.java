08a857d081bf2539429e10a9c0034dfbe51efa99
Shakir Asadov <shakirasadov@gmail.com>
2018-11-23 19:58:31 +0400

    5-th commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,35 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        int a = 5;
-        int b = 6;
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+
+        int count = 0;
+
+        int[] register = new int[100];
+        for (int r: register) {
+            r = 0;
+        }
+
+        for (int s: ar) {
+            register[s-1] += 1;
+        }
+
+        for (int r: register) {
+            count += r/2;
+        }
+
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(String.format("socks:%d", sockMerchant(9, new int[]{10, 20, 20, 10, 10, 30, 50, 10, 20})));
     }
 }
